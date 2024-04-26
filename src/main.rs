d6ebb2ed1797458fb24642ea4c520ed1f227f41d
sfgarcia <sfgarcia@uc.cl>
2024-04-26 19:47:51 -0400

    Add dependency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
+use egui_graphs::Graph;
+
 fn main() {
-    println!("Hello, world!");
-    let a = 1;
-    println!("{}", a)
+    let native_options = eframe::NativeOptions::default();
+    run_native(
+        "egui_graphs_basic_demo",
+        native_options,
+        Box::new(|cc| Box::new(BasicApp::new(cc))),
+    )
+    .unwrap();
+}
+
+pub struct BasicApp {
+    g: Graph<(), (), Directed>,
+}
+
+impl BasicApp {
+    fn new(_: &CreationContext<'_>) -> Self {
+        let g = generate_graph();
+        Self { g: Graph::from(&g) }
+    }
+}
+
+fn generate_graph() -> StableGraph<(), (), Directed> {
+    let mut g: StableGraph<(), ()> = StableGraph::new();
+
+    let a = g.add_node(());
+    let b = g.add_node(());
+    let c = g.add_node(());
+
+    g.add_edge(a, b, ());
+    g.add_edge(b, c, ());
+    g.add_edge(c, a, ());
+
+    g
+}
+
+impl App for BasicApp {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(&mut GraphView::<
+                _,
+                _,
+                _,
+                _,
+                DefaultNodeShape,
+                DefaultEdgeShape,
+            >::new(&mut self.g));
+        });
+    }
 }
