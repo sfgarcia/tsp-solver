65288b672d4ca775c988cc57a33fed1bbc9e7243
sfgarcia <sfgarcia@uc.cl>
2024-04-26 20:20:15 -0400

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,52 @@
-use eframe::{egui::{self, Context}, run_native, App, CreationContext};
-use egui_graphs::{Graph, GraphView, DefaultNodeShape, DefaultEdgeShape};
-use petgraph::{stable_graph::StableGraph, Directed};
+use plotters::prelude::*;
+use petgraph::stable_graph::StableGraph;
+use petgraph::Directed;
 
-fn main() {
-    let native_options = eframe::NativeOptions::default();
-    run_native(
-        "egui_graphs_basic_demo",
-        native_options,
-        Box::new(|cc| Box::new(BasicApp::new(cc))),
-    )
-    .unwrap();
+#[derive(Debug)]
+struct Node {
+    x: f32,
+    y: f32,
 }
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("graph.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
 
-pub struct BasicApp {
-    g: Graph<(), (), Directed>,
-}
+    let mut chart = ChartBuilder::on(&root)
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(0f32..1f32, 0f32..1f32)?;
 
-impl BasicApp {
-    fn new(_: &CreationContext<'_>) -> Self {
-        let g = generate_graph();
-        Self { g: Graph::from(&g) }
+
+    let graph = generate_graph();
+
+    for node in graph.node_indices() {
+        let point = &graph[node];
+        chart.draw_series(PointSeries::of_element(
+            [(point.x, point.y)],
+            5,
+            ShapeStyle::from(&RED).filled(),
+            &|coord, size, style| {
+                EmptyElement::at(coord)
+                    + Circle::new((0, 0), size, style)
+            },
+        ))?;
     }
+
+    Ok(())
 }
 
-fn generate_graph() -> StableGraph<(), (), Directed> {
-    let mut g: StableGraph<(), ()> = StableGraph::new();
+fn generate_graph() -> StableGraph<Node, (), Directed> {
+    let mut g: StableGraph<Node, ()> = StableGraph::new();
 
-    let a = g.add_node(());
-    let b = g.add_node(());
-    let c = g.add_node(());
+    let a = g.add_node(Node { x: 0.0, y: 0.0 });
+    let b = g.add_node(Node { x: 1.0, y: 0.0 });
+    let c = g.add_node(Node { x: 0.5, y: 1.0 });
 
     g.add_edge(a, b, ());
     g.add_edge(b, c, ());
     g.add_edge(c, a, ());
 
     g
-}
-
-impl App for BasicApp {
-    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(&mut GraphView::<
-                _,
-                _,
-                _,
-                _,
-                DefaultNodeShape,
-                DefaultEdgeShape,
-            >::new(&mut self.g));
-        });
-    }
 }
