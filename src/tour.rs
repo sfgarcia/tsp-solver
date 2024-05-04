9c69078c02fbe11c5fe42c29dea828a8483f6c6d
sfgarcia <sfgarcia@uc.cl>
2024-05-04 12:10:46 -0400

    Start refactoring

--- src/tour.rs
+++ src/tour.rs
@@ -1,10 +1,10 @@
-use petgraph::graph::{Graph, NodeIndex};
-use petgraph::Directed;
 use rand::prelude::SliceRandom;
 use rand::Rng;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct Node {
+    pub id: usize,
     pub x: f32,
     pub y: f32,
 }
@@ -12,109 +12,88 @@ pub struct Node {
 #[derive(Debug)]
 pub struct Tour {
     pub nodes: Vec<Node>,
-    pub graph: Graph<Node, (), Directed>,
+    pub route: Vec<Node>,
     pub cost: f32,
     pub distance: Vec<Vec<f32>>,
 }
 
 impl Tour {
     pub fn new(nodes: Vec<Node>) -> Self {
-        let mut graph: Graph<Node, ()> = Graph::new();
-        for node in nodes.iter() {
-        graph.add_node(node.clone());
-        }
         let mut distance = vec![vec![0.0; nodes.len()]; nodes.len()];
-        Self { nodes, graph, cost: 0.0, distance }
+        let mut route = nodes.clone();
+        route.push(nodes[0].clone());
+        Self { route, nodes, cost: 0.0, distance }
     }
 
     pub fn create_random_nodes(n: usize, width: f32, height: f32) -> Self {
         let mut rng = rand::thread_rng();
-        let mut graph: Graph<Node, ()> = Graph::new();
         let mut nodes: Vec<Node> = Vec::new();
-        for _ in 0..n {
+        let mut route: Vec<Node> = Vec::new();
+        for id in 0..n {
             let x = rng.gen::<f32>() * width;
             let y = rng.gen::<f32>() * height;
-            nodes.push(Node { x, y });
-            graph.add_node(Node { x, y });
+            route.push(Node {id, x, y });
+            nodes.push(Node {id, x, y });
         }
-        let distance = vec![vec![0.0; nodes.len()]; nodes.len()];
-        Self { nodes, graph, cost: 0.0, distance }
+        route.push(route[0].clone());
+        let distance = vec![vec![0.0; route.len()]; route.len()];
+        Self { nodes, route, cost: 0.0, distance }
     }
 
-    fn add_edge(&mut self, a: usize, b: usize) {
-        let a = self.graph.node_indices().nth(a).unwrap();
-        let b = self.graph.node_indices().nth(b).unwrap();
-        self.graph.add_edge(a, b, ());
-    }
-
-    fn add_edges(&mut self, edges: Vec<(usize, usize)>) {
-        for (a, b) in edges {
-            self.add_edge(a, b);
-        }
-    }
-
-    fn distance(&self, a: usize, b: usize) -> f32 {
-        let node_a = &self.nodes[a];
-        let node_b = &self.nodes[b];
-        ((node_a.x - node_b.x).powi(2) + (node_a.y - node_b.y).powi(2)).sqrt()
-    }
-
-    fn get_node(&self, index: usize) -> NodeIndex {
-        self.graph.node_indices().nth(index).unwrap()
+    fn distance(&self, node_1: &Node, node_2: &Node) -> f32 {
+        ((node_1.x - node_2.x).powi(2) + (node_1.y - node_2.y).powi(2)).sqrt()
     }
 
     pub fn distance_matrix(&mut self) {
-        for i in 0..self.nodes.len() {
-            for j in 0..self.nodes.len() {
-                self.distance[i][j] = self.distance(i, j);
+        for node_1 in self.nodes.iter() {
+            for node_2 in self.nodes.iter() {
+                self.distance[node_1.id][node_2.id] = self.distance(node_1, node_2);
             }
         }
     }
 
     fn calculate_cost(&mut self) {
         let mut cost = 0.0;
-        for edge in self.graph.edge_indices() {
-            let (start_node, end_node) = self.graph.edge_endpoints(edge).unwrap();
-            cost += self.distance(start_node.index(), end_node.index());
+        for i in 0..self.route.len() - 1 {
+            cost += self.distance(&self.route[i], &self.route[i + 1]);
         }
         self.cost = cost;
     }
 
     pub fn random_tour(&mut self) {
-        let mut indices: Vec<usize> = (1..self.nodes.len()).collect();
-        indices.shuffle(&mut rand::thread_rng());
-        // Remove and return the first element of the indices vector
-        let tour_init = 0;
-        let mut current_index = tour_init;
-        while indices.len() > 0 {
-            let next_index = indices.remove(0);
-            self.add_edge(current_index, next_index);
-            current_index = next_index;
-        }
-        self.add_edge(current_index, tour_init);
+        let mut route: Vec<Node> = self.nodes[1..].to_vec();
+        route.shuffle(&mut rand::thread_rng());
+        self.route = vec![self.nodes[0].clone()];
+        self.route.extend(route);
+        self.route.push(self.nodes[0].clone());
     }
 
     pub fn nearest_neighbour_tour(&mut self) {
         self.distance_matrix();
-        let tour_init = 0;
-        let mut current_index = tour_init;
-        let mut visited = vec![false; self.nodes.len()];
-        visited[current_index] = true;
+        let mut current_index = self.nodes[0].id;
+        let mut route = vec![self.nodes[0].clone()];
+        let mut visited: HashMap<usize, bool> = HashMap::new();
+        for node in self.nodes.iter() {
+            visited.insert(node.id, false);
+        }
+        visited[&current_index] = true;
         for _ in 0..self.nodes.len() - 1 {
             let mut dist: Vec<f32> = self.distance[current_index].clone();
             for i in 0..self.nodes.len() {
-                if visited[i] {
+                if visited[&i] {
                     dist[i] = f32::MAX;
                 }
             }
             let min_index = dist.iter().enumerate().min_by(|x, y| x.1.partial_cmp(y.1).unwrap()).unwrap().0;
-            self.add_edge(current_index, min_index);
-            visited[min_index] = true;
+            route.push(self.nodes[min_index].clone());
+            visited[&min_index] = true;
             current_index = min_index;
         }
-        self.add_edge(current_index, tour_init);
+        route.push(self.nodes[0].clone());
+        self.route = route;
     }
 
+    /*
     pub fn two_opt(&mut self) {
         let mut improved = true;
         while improved {
@@ -159,4 +138,5 @@ impl Tour {
         }
         self.graph = new_graph;
     }
+     */
 }
