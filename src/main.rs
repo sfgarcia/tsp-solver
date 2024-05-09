a78cc72ab30793046db336a1ec2523c6209a6bda
sfgarcia <sfgarcia@uc.cl>
2024-05-09 11:23:00 -0400

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
 use plotters::backend::RGBPixel;
 use plotters::coord::types::RangedCoordf32;
+use plotters::coord::Shift;
 use plotters::prelude::*;
 use crate::tour::Tour;
 use crate::tour::Node;
-use crate::visualization::Visualization;
 
 pub mod tour;
-pub mod visualization;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (width, height) = (640, 480);
-    let root = BitMapBackend::new("graph.png", (width, height)).into_drawing_area();
-    root.fill(&WHITE)?;
-
+fn main(){
     let route = generate_random_graph();
+    plot_route(&route, 640, 480, "graph.png");
+}
+
+fn plot_route(route: &Vec<Node>, width: u32, height: u32, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(path, (width, height)).into_drawing_area();
+    root.fill(&WHITE)?;
 
     // Find the minimum and maximum x and y values in the graph
     let min_x = route.iter().map(|node| node.x).fold(f32::INFINITY, |a, b| a.min(b));
     let max_x = route.iter().map(|node| node.x).fold(f32::NEG_INFINITY, |a, b| a.max(b));
     let min_y = route.iter().map(|node| node.y).fold(f32::INFINITY, |a, b| a.min(b));
     let max_y = route.iter().map(|node| node.y).fold(f32::NEG_INFINITY, |a, b| a.max(b));
-
-    // Add a margin to the drawing area
-    let margin = 40;
-    let drawing_width = width - 2 * margin;
-    let drawing_height = height - 2 * margin;
 
     let mut chart = ChartBuilder::on(&root)
         .margin(5)
@@ -34,6 +30,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     draw_edges(&mut chart, &route)?;
 
+    // Add a margin to the drawing area
+    let margin = 40;
+    let drawing_width = width - 2 * margin;
+    let drawing_height = height - 2 * margin;
+
+    draw_nodes(&root, &mut chart, &route, min_x, max_x, min_y, max_y, drawing_width, drawing_height, margin, height)?;
+    Ok(())
+}
+
+fn draw_edges(chart: &mut ChartContext<BitMapBackend<RGBPixel>, Cartesian2d<RangedCoordf32, RangedCoordf32>>, route: &Vec<Node>) -> Result<(), Box<dyn std::error::Error>> {
+    for i in 0..route.len() - 1 {
+        // Draw the edge as a line between the start node and the end node
+        chart.draw_series(LineSeries::new(
+            vec![(route[i].x, route[i].y), (route[i + 1].x, route[i + 1].y)],
+            &BLACK,
+        ))?;
+    }
+    Ok(())
+}
+
+fn draw_nodes(
+        root: &DrawingArea<BitMapBackend<RGBPixel>, Shift>,
+        chart: &mut ChartContext<BitMapBackend<RGBPixel>, Cartesian2d<RangedCoordf32, RangedCoordf32>>,
+        route: &Vec<Node>,
+        min_x: f32,
+        max_x: f32,
+        min_y: f32,
+        max_y: f32,
+        drawing_width: u32,
+        drawing_height: u32,
+        margin: u32,
+        height: u32,
+        ) -> Result<(), Box<dyn std::error::Error>> {
     for node in route.iter() {
         chart.draw_series(PointSeries::of_element(
             [(node.x, node.y)],
@@ -49,18 +78,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let label_coord = ((node.x - min_x) / (max_x - min_x) * drawing_width as f32 + margin as f32, (node.y - min_y) / (max_y - min_y) * drawing_height as f32 + margin as f32);
         let text_style = TextStyle::from(("Arial", 15).into_font()).color(&BLACK);
         root.draw_text(&format!("Node {}", node.id), &text_style, (label_coord.0 as i32, (height as f32 - label_coord.1) as i32 - 20))?;
-    }
-
-    Ok(())
-}
-
-fn draw_edges(chart: &mut ChartContext<BitMapBackend<RGBPixel>, Cartesian2d<RangedCoordf32, RangedCoordf32>>, route: &Vec<Node>) -> Result<(), Box<dyn std::error::Error>> {
-    for i in 0..route.len() - 1 {
-        // Draw the edge as a line between the start node and the end node
-        chart.draw_series(LineSeries::new(
-            vec![(route[i].x, route[i].y), (route[i + 1].x, route[i + 1].y)],
-            &BLACK,
-        ))?;
     }
     Ok(())
 }
--- src/visualization.rs
+++ src/visualization.rs
@@ -14,14 +14,12 @@ pub struct Visualization<'a> {
     pub max_x: f32,
     pub min_y: f32,
     pub max_y: f32,
-    pub root: &'a DrawingArea<BitMapBackend<'a, RGBPixel>, Shift>,
     pub chart: ChartContext<'a, BitMapBackend<'a, RGBPixel>, Cartesian2d<RangedCoordf32, RangedCoordf32>>,
     route: Vec<Node>,
 }
 
 impl<'a> Visualization<'a> {
-    pub fn new(path: &'a str, width: u32, height: u32, margin: u32, route: Vec<Node>) -> Self {
-        let root = BitMapBackend::new(path, (width, height)).into_drawing_area();
+    pub fn new(root: DrawingArea<BitMapBackend<'static, RGBPixel>, Shift>, width: u32, height: u32, margin: u32, route: Vec<Node>) -> Self {
         root.fill(&WHITE).unwrap();
 
         // Find the minimum and maximum x and y values in the graph
@@ -30,12 +28,12 @@ impl<'a> Visualization<'a> {
         let min_y = route.iter().map(|node| node.y).fold(f32::INFINITY, |a, b| a.min(b));
         let max_y = route.iter().map(|node| node.y).fold(f32::NEG_INFINITY, |a, b| a.max(b));
 
-        let chart = ChartBuilder::on(&root)
+        let chart = ChartBuilder::on(&root.clone())
             .caption("Travelling Salesman Problem", ("Arial", 30).into_font())
             .x_label_area_size(40)
             .y_label_area_size(40)
             .build_cartesian_2d(min_x..max_x, min_y..max_y)
             .unwrap();
-        Self { width, height, margin, drawing_width: width - 2 * margin, drawing_height: height - 2 * margin, min_x, max_x, min_y, max_y, root: &root, chart, route }
+        Self { width, height, margin, drawing_width: width - 2 * margin, drawing_height: height - 2 * margin, min_x, max_x, min_y, max_y, chart, route }
     }
 }
